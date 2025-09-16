7e261074f0a3da4c13510efd35213fef4a74d14b
Eron <aarondamaolao13@gmail.com>
2025-09-16 23:49:25 +0800

    adt variation 3

--- arraylist/array_var3.c
+++ arraylist/array_var3.c
@@ -9,46 +9,45 @@ typedef struct{
 }List;
 
 List initialize(List L);
-List insertPos(List L, int position, int data, int position);
+List insertPos(List L, int data, int position);
 List deletePos(List L, int position);
 int locate(List L, int data);
 List insertSorted(List L, int data);
 void display(List L);
 List resize(List L);
 
-
 int main(){
-
+    List L = initialize(L);
+    
+    L = insertPos(L, 20, 0);
+    L = insertPos(L, 30, 1);
+    L = insertSorted(L, 25);
+    
+    display(L);
+    
+    return 0;
+    
 }
 
 List initialize(List L){
-    L.elemPtr = (int *)malloc(LENGTH * sizeof(int));
-    L.count = 0;
+    L.elemPtr =(int *)malloc(LENGTH * sizeof(int));
     L.max = LENGTH;
-
-    for(int i = 0; i < L.max; i++){
-        L.elemPtr[i] = -1;
-    }
-
+    L.count = 0;
     return L;
 }
 
-List insertPos(List L, int position, int data){
+List insertPos(List L, int data, int position){
+    if(position < 0 || position > L.count){
+        printf("Invalid Position");
+        exit(1);
+    }
     if(L.count >= L.max){
+        printf("Array is full!");
         L = resize(L);
-        if(L.max == -1){
-            printf("Memory allocation failed!\n");
-            return L;
-        }
     }
-
-    if(position < 0 || position >= L.count){
-        printf("Invalid Position!\n");
-        return L;
-    }
-
-    for(int i = L.count; i > position; i--){
-        L.elemPtr[i] = L.elemPtr[i - 1];
+    
+    for(int i = L.count - 1; i >= position; i--){
+        L.elemPtr[i + 1] = L.elemPtr[i];
     }
     L.elemPtr[position] = data;
     L.count++;
@@ -56,21 +55,13 @@ List insertPos(List L, int position, int data){
 }
 
 List deletePos(List L, int position){
-    if(L.count == 0){
-        printf("The list is empty!\n");
-        return L;
-    }
-
     if(position < 0 || position >= L.count){
-        printf("Invalid Position!\n");
-        return L;
+        printf("Invalid Position");
+        exit(1);
     }
-
-    for(int i = position; i < L.count -1; i++){
+    for(int i = position; i < L.count - 1; i++){
         L.elemPtr[i] = L.elemPtr[i + 1];
     }
-
-    L.elemPtr[L.count - 1] = -1;
     L.count--;
     return L;
 }
@@ -86,38 +77,31 @@ int locate(List L, int data){
 
 List insertSorted(List L, int data){
     if(L.count >= L.max){
+        printf("Array is full");
         L = resize(L);
-        if(L.max == -1){
-            printf("Memory allocation failed!\n");
-            return L;
-        }
     }
-
     int i;
-    for(i = L.count -1;(i >= 0 && L.elemPtr[i] > data); i--){
+    for(i = L.count - 1;(i >= 0 && L.elemPtr[i] > data);i--){
         L.elemPtr[i + 1] = L.elemPtr[i];
     }
-
     L.elemPtr[i + 1] = data;
     L.count++;
     return L;
 }
 
-void display(List L){
-    if(L.count == 0){
-        printf("The list is empty!\n");
-        return;
+List resize(List L){
+    L.max = L.max * 2;
+    L.elemPtr = (int *)realloc(L.elemPtr, L.max * sizeof(int));
+    if(L.elemPtr == NULL){
+        printf("Memory allocation failed!");
+        exit(1);
     }
+    return L;
+}
 
-    printf("List elements: ");
+void display(List L){
+    printf("Elements: ");
     for(int i = 0; i < L.count; i++){
         printf("%d ", L.elemPtr[i]);
     }
-    printf("\n");
-}
-
-List resize(List L){
-    int newMax = L.max * 2;
-    int *newArray = (int *)malloc(newMax * sizeof(int));
-    return L;
 }
