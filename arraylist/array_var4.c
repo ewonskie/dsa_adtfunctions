7237644dfbc91596ffe9c6c2a3ba9f0f8d4341b3
Eron <aarondamaolao13@gmail.com>
2025-09-16 21:02:25 +0800

    adt list variations

--- arraylist/array_var4.c
+++ arraylist/array_var4.c
@@ -12,7 +12,7 @@ void initialize(List *L);
 void insertPos(List *L, int data, int position);
 void deletePos(List *L, int position);
 int locate(List *L, int data);
-int retrive(List *L, int position);
+int retrieve(List *L, int position);
 void insertSorted(List *L, int data);
 void display(List *L);
 void resize(List *L);
@@ -20,7 +20,14 @@ void makeNULL(List *L);
 
 
 int main(){
-
+    List L; 
+    initialize(&L);
+    insertPos(&L, 10, 0);
+    insertPos(&L, 20, 1);
+    insertPos(&L, 30, 1);
+    display(&L);
+    return 0;
+    
 
 }
 
@@ -78,3 +85,39 @@ int retrieve(List *L, int position){
 
     return L-elemPtr[position];
 }
+
+void insertSorted(List *L, int data){
+    if(L->count >= L->max){
+        resize(L);
+    }
+
+    int i;
+    for(int i = L.count - 1; (i >= 0 && L->elemPtr[i] > data); i--){
+        L->elemPtr[i + 1] = L->elemPtr[i];
+    })
+
+    L->elemPtr[i + 1] = data;
+    L->count++;
+}
+
+void display(List *L){
+    printf("List elements: ");
+    for(int i = 0; i < L->count; i++){
+        printf("%d ", L->elemPtr[i]);
+    }
+    printf("\n");
+}
+
+void resize(List *L){
+    int newMax = L->max * 2;
+    int newArr = (int *)malloc(newMax * sizeof(int));
+}
+
+void makeNULL(List *L){
+    free(L->elemPtr);
+    L->elemPtr = NULL;
+    L->count = 0;
+    L->max = 0;
+}
+
+
